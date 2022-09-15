2676868880988ef8618ecd9dec1618d8f41d34e6
Renan <rgrenantd3@gmail.com>
2022-09-15 16:03:09 -0300

    Exercicio Concluido

---
+++ src/script.ts
@@ -0,0 +1,10 @@
+var idade: number = 27;
+var nome: string = 'diogo';
+var maiorDeIdade: boolean = true
+
+function soma (num1: number, num2: number): number{
+    var soma = num1 + num2;
+    return soma;
+}
+document.write (`A soma de 7+5 é = ${soma(7, 5)}`);
+
